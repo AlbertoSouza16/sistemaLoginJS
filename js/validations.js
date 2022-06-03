2701ff2dbd0484f9b6c95f89ed420a59bf681879
Alberto Souza <albertosouza16.contato@gmail.com>
2022-06-03 17:12:15 -0300

    all labels validation

--- js/validations.js
+++ js/validations.js
@@ -24,6 +24,42 @@ name.addEventListener('keyup', ()=>{
     }
 })
 
+login.addEventListener('keyup', ()=>{
+    if(login.value.length <= 4){
+        labelLogin.setAttribute('style', 'color: red');
+        labelLogin.innerHTML = '<strong>LOGIN * enter at least 5 characters </strong>'
+        login.setAttribute('style', 'border-color: red');
+    }else{
+        labelLogin.setAttribute('style', 'color: green');
+        labelLogin.innerHTML = 'LOGIN'
+        login.setAttribute('style', 'border-color: green');
+    }
+})
+
+pass.addEventListener('keyup', ()=>{
+    if(pass.value.length <= 7){
+        labelPass.setAttribute('style', 'color: red');
+        labelPass.innerHTML = '<strong>PASSWORD * enter at least 8 characters </strong>'
+        pass.setAttribute('style', 'border-color: red');
+    }else{
+        labelPass.setAttribute('style', 'color: green');
+        labelPass.innerHTML = 'PASSWORD'
+        pass.setAttribute('style', 'border-color: green');
+    }
+})
+
+confPass.addEventListener('keyup', ()=>{
+    if(pass.value != confPass.value){
+        labelConfPass.setAttribute('style', 'color: red');
+        labelConfPass.innerHTML = '<strong>CONFIRM PASSWORD * passwords do not match </strong>'
+        confPass.setAttribute('style', 'border-color: red');
+    }else{
+        labelConfPass.setAttribute('style', 'color: green');
+        labelConfPass.innerHTML = 'CONFIRM PASSWORD'
+        confPass.setAttribute('style', 'border-color: green');
+    }
+})
+
 
 function register(){
     

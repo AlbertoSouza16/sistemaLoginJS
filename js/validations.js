4943af1c283f032e514dafe3783e3645d5f74342
Alberto Souza <albertosouza16.contato@gmail.com>
2022-06-03 16:57:15 -0300

    label validations

--- js/validations.js
+++ js/validations.js
@@ -12,7 +12,19 @@ let confPass = document.querySelector('#confPass');
 let labelConfPass = document.querySelector('#labelConfPass');
 
 
+name.addEventListener('keyup', ()=>{
+    if(name.value.length <= 3){
+        labelName.setAttribute('style', 'color: red');
+        labelName.innerHTML = '<strong>FULL NAME * enter at least 3 characters </strong>'
+        name.setAttribute('style', 'border-color: red');
+    }else{
+        labelName.setAttribute('style', 'color: green');
+        labelName.innerHTML = 'FULL NAME'
+        name.setAttribute('style', 'border-color: green');
+    }
+})
+
 
 function register(){
-    alert("funfou");
+    
 }

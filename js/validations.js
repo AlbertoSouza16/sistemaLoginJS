c1816673b6a9e91efe0711d45d84400aa884fd89
Alberto Souza <albertosouza16.contato@gmail.com>
2022-06-03 16:43:56 -0300

    js changes

---
+++ js/validations.js
@@ -0,0 +1,18 @@
+//variables
+let name = document.querySelector('#name');
+let labelName = document.querySelector('#labelName');
+
+let login = document.querySelector('#login');
+let labelLogin = document.querySelector('#labelLogin');
+
+let pass = document.querySelector('#pass');
+let labelPass = document.querySelector('#labelPass');
+
+let confPass = document.querySelector('#confPass');
+let labelConfPass = document.querySelector('#labelConfPass');
+
+
+
+function register(){
+    alert("funfou");
+}

77dc1c5e3330c2d907a9d13e5ce344dd17c515ee
Alberto Souza <albertosouza16.contato@gmail.com>
2022-06-05 01:46:23 -0300

    final commit

--- js/exit.js
+++ js/exit.js
@@ -2,3 +2,8 @@ function exit(){
     localStorage.removeItem('token')
     window.location.href = "index.html"
 }
+
+if(localStorage.getItem('token') == null){
+    alert("You must be logged in to continue")
+    window.location.href = "index.html"
+}
